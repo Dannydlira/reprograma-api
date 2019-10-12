c1f60fa19ccbf85ad230deb624a8ecd68ca5ad5b
Dannydlira <dannydlira@gmail.com>
2019-10-11 21:49:30 -0300

    nova atualização da api

---
+++ src/controllers/professorasController.js
@@ -0,0 +1,29 @@
+const professoras = require('../model/professoras.json')
+    
+exports.get = (req, res) => {
+    const arrProfs =[]
+    for (let i = 0; i < professoras.length; i++) {
+        const semCpf = {}
+        semCpf.id = professoras[i].id
+        semCpf.nome = professoras[i].nome
+        semCpf.especialidade = professoras[i].especialidade
+        semCpf.signo = professoras[i].signo
+        arrProfs.push(semCpf) 
+    }
+    console.log(req.url)
+    
+    res.status(200).send(arrProfs)
+}
+
+//  exports.get = (req, res) => {
+//     const profSemCpf = professoras.map(item => {) 
+//         item.cpf = "************"
+//         return item
+
+//     }
+
+// const profSemCpf = professoras.map(item => { 
+//    delete item.cpf
+//    return item
+// })   
+    
